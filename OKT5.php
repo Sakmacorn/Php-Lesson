eff494f955e17e3b71985943d358f222b394782f
Miguel GitHub <miguellulu34@gmail.com>
2025-07-17 09:39:43 +0800

    Update OKT5.php

--- OKT5.php
+++ OKT5.php
@@ -1,34 +1,23 @@
 <?php
 
-//Option 1
-$add = 23;
-$sub = 22;
-$mul = 5;
-$div = 10;
+$a = 10;
+$b = 20;
+$c = 30;
+$d = 40;
 
- echo "Addition: ". ($add + $sub);   
- echo "<br>The minimum in mabalacat is ". ($add / $div);
+$add = $a + $b;
+$sub = $c - $b;
+$mul = $a * $d;
+$div = $d / $b;
+$total = $add + $sub + $mul + $div;
+$average = $total / 4;
 
+echo "Addition of $a and $b: ". $add;
+echo "<br> Subtraction of $c and $b: ". $sub;
+echo "<br> Multiplication of $a and $d: ". $mul;
+echo "<br> Division of $d and $b: ". $div;
 
- //Option 2
-$tax = 0.15;
-$gross_pay = 5000;
-
-$tax_amount = $gross_pay * $tax;
-
-echo "<br> Your Tax Deduction is: ". $tax_amount;
-
-//Option 3
-$base_amount = 500;
-$percentage = 15;
-
-$tax_rate = $percentage / 100;
-
-$tax_amount = $base_amount * $tax_rate;
-
-$total = $base_amount + $tax_amount;
-
-echo "<br>Base amount: $". $base_amount;
-echo "<br>Tax Percentage: ". $tax_rate;
+echo "<br>Total of all operations: ". $total;
+echo "<br>Average of all results: ". $average;
 
 ?>
