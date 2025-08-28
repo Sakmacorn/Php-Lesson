bb49ceda5fc70d08594bd1d41b60c0e00f59843e
Miguel GitHub <miguellulu34@gmail.com>
2025-08-28 08:28:42 +0800

    Add files via upload
    Computation Example

--- lulu/compute.php
+++ lulu/compute.php
@@ -1,8 +1,19 @@
 <?php
-$x = $_POST['num1'];
-$y = $_POST['num2'];
 
-$Sum = $x + $y;
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$num3 = $_POST['num3'];
+$num4 = $_POST['num4'];
 
-echo "<br>The sum is $Sum";
+$sum = $num1 + $num2;
+$diff = $num4 - $num3;
+$prod = $num1 * $num3;
+$quo = $num2 / $num3;
+$ave = ($num1 + $num2 + $num3 + $num4) / 4;
+
+echo "The sum of $num1 and $num2 is $sum <br>"; 
+echo "The difference of $num4 and $num3 is $diff <br>";
+echo "The product of $num1 and $num3 is $prod <br>";
+echo "The quotient of $num2 and $num3 is ".round($quo, 2);
+echo "<br>The average of $num1, $num2, $num3 and $num4 is $ave";
 ?>
---
+++ lulu/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Example Compute</title>
+</head>
+<body>
+    <form action="compute.php" method="post">
+        <label for="">Enter First Number: </label> 
+        <input type="number1" name="num1" id=""> <br><br>
+        <label for="">Enter Second Number: </label>
+        <input type="number2" name="num2" id=""> <br><br>
+        <label for="">Enter Third Number: </label>
+        <input type="number3" name="num3" id=""> <br><br>
+        <label for="">Enter Fourth Number: </label> 
+        <input type="number4" name="num4" id=""> <br><br>
+        <input type="submit" value="Compute">
+    </form>
+</body>
+</html>
